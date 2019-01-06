dd4c9c9c4896bfb33dac41470f665f54d93158c6
Sophie Li <sophcli@gmail.com>
2019-01-06 21:43:11 +0800

    finalised homepage design and cleared all major bugs

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,10 +2,11 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["Cheers!", "Find Your Poison", "To raise your spirits", "Shaken, not stirred"],
+    strings: ["Cheers!", "Pick Your Poison", "Raise your spirits", "Shaken, not stirred"],
     typeSpeed: 50,
     loop: true
   });
 }
 
 export { loadDynamicBannerText };
+
