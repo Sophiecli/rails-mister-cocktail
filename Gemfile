1ec057ecae955d83a30648abad6b2e0d15cf331b
Sophie Li <sophcli@gmail.com>
2019-01-09 15:53:49 +0800

    added seeds

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'simple_form'
 # 3rd party platforms - to store stuff
 gem 'cloudinary'
 gem 'carrierwave', '~> 1.2'
-
+gem 'faker'
 gem 'rest-client', '>= 2.0'
 
 # Use ActiveStorage variant
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,12 @@
 
 require 'json'
 require 'open-uri'
+# require 'faker'
 
-puts "Cleaning DB"
+puts "Cleansing out ingredient database..."
 Ingredient.destroy_all
 
-puts "Generating DB"
+puts "Generating new ingredient database..."
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredient_serialized = open(url).read
@@ -22,4 +23,20 @@ ingredient['drinks'].each do |drink|
   Ingredient.create(name: drink["strIngredient1"])
 end
 
-puts "DB generated"
+puts "Ingredients database has been generated"
+
+Cocktail.destroy_all
+
+# Cloudinary photos
+photos = ['https://res.cloudinary.com/sophiecli/image/upload/v1542347238/Mister%20Cocktail%20App/whiskeysour.jpg',
+          'https://res.cloudinary.com/sophiecli/image/upload/v1542347208/Mister%20Cocktail%20App/gintonic.jpg',
+          'https://res.cloudinary.com/sophiecli/image/upload/v1547019026/Mister%20Cocktail%20App/alcoholic-beverage-ale-beer-1323593.jpg',
+          'https://res.cloudinary.com/sophiecli/image/upload/v1547019027/Mister%20Cocktail%20App/alcoholic-bar-beverage-338713.jpg',
+          'https://res.cloudinary.com/sophiecli/image/upload/v1542347224/Mister%20Cocktail%20App/pinkpanther.jpg']
+
+puts "Adding Cocktails"
+5.times do
+  drink = Cocktail.create(name: Faker::Beer.name)
+  drink.remote_photo_url = photos.sample
+  drink.save
+end
